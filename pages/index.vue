3426415ea7593a02cfc83a3ae05784a88d283d1f
bbjansen <bobjansen@pm.me>
2020-01-07 18:52:25 +0100

    index layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,65 @@
 <template>
-  <v-layout
+<div>
+  <v-row
     column
     justify-center
     align-center
   >
-    <v-flex
+    <v-col
       xs12
       sm8
       md6
     >
       <v-card
         class="mx-auto"
-        width="960"
+        width="100%"
       >
         <v-card-title class="title" />
         <v-card-text />
         <v-card-actions />
       </v-card>
-    </v-flex>
-  </v-layout>
+
+    </v-col>
+  </v-row>
+  <v-row
+    column
+    justify-center
+    align-center
+  >
+    <v-col
+      xs6
+      sm6
+      md6
+    >
+      <v-card
+        class="mx-auto"
+        width="100%"
+      >
+        <v-card-title class="title" />
+        <v-card-text />
+        <v-card-actions />
+      </v-card>
+
+    </v-col>
+
+    <v-col
+      xs6
+      sm6
+      md6
+    >
+      <v-card
+        class="mx-auto"
+        width="100%"
+      >
+        <v-card-title class="title" />
+        <v-card-text />
+        <v-card-actions />
+      </v-card>
+
+    </v-col>
+  </v-row>
+
+</div>
 </template>
 
 <script>
