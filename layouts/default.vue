393306b7bfca5eea1dcd6b525b23b953a9bf2c91
bbjansen <bobjansen@pm.me>
2020-01-07 18:45:52 +0100

    center stat panel

--- layouts/default.vue
+++ layouts/default.vue
@@ -57,8 +57,7 @@
               </v-btn>
             </v-toolbar-items>
 
-            <v-toolbar-items
-            >
+            <v-toolbar-items>
               <v-select
                 :items="dropdown"
                 text
@@ -72,49 +71,50 @@
                 flat
                 hide-details
                 hide-selected
-
               />
             </v-toolbar-items>
           </v-toolbar>
 
-          <v-container>
-            <v-row justify="center">
-              <v-col cols="6" md="6">
-                <v-text-field
-                  solo-inverted
-                  hide-details
-                  flat
-                  rounded
-                  label="Search for a transaction ID, address or block"
-                  prepend-inner-icon="mdi-magnify"
-                  background-color="#30195d"
-                  color="#6e5f8e"
-                />
-              </v-col>
+          <v-row justify="center">
+            <v-col cols="6" md="6">
+              <v-text-field
+                solo-inverted
+                hide-details
+                flat
+                rounded
+                label="Search for a transaction ID, address or block"
+                prepend-inner-icon="mdi-magnify"
+                background-color="#30195d"
+                color="#6e5f8e"
+              />
+            </v-col>
 
-              <v-col cols="2" md="2">
-                <v-btn
-                  outlined
-                  rounded
-                  flat
-                  color="#30195d"
-                  height="50"
-                >
-                  Calculator
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
+            <v-col cols="2" md="2">
+              <v-btn
+                outlined
+                rounded
+                flat
+                color="#30195d"
+                height="50"
+              >
+                Calculator
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-container>
       </v-toolbar>
 
-      <v-card
-        class="mx-auto"
-        max-width="960"
-        style="margin-top: -35px;"
-      >
-        <v-card-text style="height: 100px;" />
-      </v-card>
+      <v-container>
+        <v-card
+          class="mx-auto"
+          max-width="960"
+          style="margin-top: -35px;"
+        >
+          <v-card-title class="title" />
+          <v-card-text />
+          <v-card-actions />
+        </v-card>
+      </v-container>
     </v-card>
 
     <v-content>
